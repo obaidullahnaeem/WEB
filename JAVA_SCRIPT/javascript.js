095afedd43fa4b803a1f808a1bdfaee9344d50f4
obaidullahnaeem <obaidullahnaeem55@gmail.com>
2026-08-12 14:34:31 +0500

    started java script ;earned consolve log and some data types

---
+++ JAVA_SCRIPT/javascript.js
@@ -0,0 +1,24 @@
+{/* <script src="javascript.js"></script> */}
+// that how you can add a javascript file to your html file
+
+
+
+// let firstName = "John";
+// let lastName = "Doe";
+
+
+
+// let age = 11;
+
+// age = 54;
+
+
+
+// const pi = 3.14;
+// pi = 10;
+
+// var max=57
+// var actual= max-13
+// var percentage= actual/max*100
+
+let message = 'Hello!'; // define the variable and assign the value
